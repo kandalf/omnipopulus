62004adb4aebb827a06430fe12843b0f5fcbd34a
Tim Riley <tim@openmonkey.com>
2010-10-10 16:33:45 +1100

    Add the more standard "Sign in with Twitter/Facebook" buttons, including helpers to insert them. Encapsulate the twitter and facebook auth URLs in an auth_request_path helper.

--- app/helpers/omnisocial/auth_helper.rb
+++ app/helpers/omnisocial/auth_helper.rb
@@ -1,11 +1,23 @@
 module Omnisocial
   module AuthHelper
-    def twitter_login_button
-      content_tag(:a, content_tag(:span, 'Sign in with Twitter'), :class => 'twitter', :href => '/auth/twitter')
+    def auth_request_path(options = {})
+      "/auth/#{options[:service]}"
+    end
+    
+    def big_twitter_login_button
+      content_tag(:a, content_tag(:span, 'Sign in with Twitter'), :class => 'twitter', :href => auth_request_path(:service => 'twitter'))
     end
   
+    def big_facebook_login_button
+      content_tag(:a, content_tag(:span, 'Sign in with Facebook'), :class => 'facebook', :href => auth_request_path(:service => 'facebook'))
+    end
+    
+    def twitter_login_button
+      content_tag(:a, content_tag(:img, :src => '/images/omnisocial/signin_twitter.png', :alt => 'Sign in with Twitter'), :href => auth_request_path(:service => 'twitter'))
+    end
+    
     def facebook_login_button
-      content_tag(:a, content_tag(:span, 'Sign in with Facebook'), :class => 'facebook', :href => '/auth/facebook')
+      content_tag(:a, content_tag(:img, :src => '/images/omnisocial/signin_facebook.png', :alt => 'Sign in with Facebook'), :href => auth_request_path(:service => 'facebook'))
     end
   end
 end
--- lib/generators/omnisocial/omnisocial_generator.rb
+++ lib/generators/omnisocial/omnisocial_generator.rb
@@ -32,8 +32,10 @@ module Omnisocial
       
       def copy_assets
         copy_file 'assets/stylesheets/omnisocial.css',  'public/stylesheets/omnisocial.css'
-        copy_file 'assets/images/twitter.gif',          'public/images/twitter.gif'
-        copy_file 'assets/images/facebook.png',         'public/images/facebook.png'
+        copy_file 'assets/images/twitter.gif',          'public/images/omnisocial/twitter.gif'
+        copy_file 'assets/images/facebook.png',         'public/images/omnisocial/facebook.png'
+        copy_file 'assets/images/signin_twitter.png',   'public/images/omnisocial/signin_twitter.png'
+        copy_file 'assets/images/signin_facebook.png',  'public/images/omnisocial/signin_facebook.png'
       end
       
       def show_readme
