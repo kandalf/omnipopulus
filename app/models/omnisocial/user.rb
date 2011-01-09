feb7c42e91020e8a0e5608b2dbc495e6faaaf184
Tim Riley <tim@openmonkey.com>
2011-01-10 10:49:26 +1100

    Make Omnisocial::User an abstract class, since the generator creates a User class that inherits from it anyway. That in-app User class (not Omnisocial::User) should be considered the base_class for STI purposes and other such things. Thanks to carhartl (https://github.com/carhartl) for spotting this.

--- app/models/omnisocial/user.rb
+++ app/models/omnisocial/user.rb
@@ -1,5 +1,7 @@
 module Omnisocial
   class User < ActiveRecord::Base
+    self.abstract_class = true
+    
     has_one :login_account, :class_name => 'Omnisocial::LoginAccount', :dependent => :destroy
     delegate :login, :name, :picture_url, :account_url, :access_token, :to => :login_account
 
