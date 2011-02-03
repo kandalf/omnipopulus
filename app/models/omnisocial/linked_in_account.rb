8f1ecce5e44aeb5b65bf348f7fe758c7a7f869b2
Tim Riley <tim@openmonkey.com>
2011-02-04 10:09:05 +1100

    Save the access token secret for LinkedIn accounts. Since LinkedIn uses OAuth 1.0, I'm pretty sure this will be needed.

--- app/models/omnisocial/linked_in_account.rb
+++ app/models/omnisocial/linked_in_account.rb
@@ -5,6 +5,7 @@ module Omnisocial
       self.picture_url         = auth_hash['user_info']['image']
       self.name                = auth_hash['user_info']['first_name']
       self.access_token        = auth_hash['credentials']['token']
+      self.access_token_secret = auth_hash['credentials']['secret']
     end
   
     def account_url
