95c17c3afa3f5b8dcd6378c3cc104724f501de28
Tim Riley <tim@openmonkey.com>
2011-02-16 06:25:08 +1100

    Since we've removed the big images for the service login buttons, rename the helper methods for these buttons appropriately: big_twitter_login_button -> twitter_login_button, etc.

--- app/helpers/omnisocial/auth_helper.rb
+++ app/helpers/omnisocial/auth_helper.rb
@@ -4,19 +4,19 @@ module Omnisocial
       "/auth/#{options[:service]}"
     end
 
-    def big_twitter_login_button
+    def twitter_login_button
       content_tag(:a, content_tag(:span, 'Sign in with Twitter'), :class => 'omnisocial-button twitter', :href => auth_request_path(:service => 'twitter'))
     end
 
-    def big_facebook_login_button
+    def facebook_login_button
       content_tag(:a, content_tag(:span, 'Sign in with Facebook'), :class => 'omnisocial-button facebook', :href => auth_request_path(:service => 'facebook'))
     end
 
-    def big_linkedin_login_button
+    def linkedin_login_button
       content_tag(:a, content_tag(:span, 'Sign in with LinkedIn'), :class => 'omnisocial-button linkedin', :href => auth_request_path(:service => 'linked_in'))
     end
 
-    def big_github_login_button
+    def github_login_button
       content_tag(:a, content_tag(:span, 'Sign in with Github'), :class => 'omnisocial-button github', :href => auth_request_path(:service => 'github'))
     end
   end
