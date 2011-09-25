3056045d50154a62171141c2bff050158a4e2e45
Leonardo Mateo <leonardomateo@gmail.com>
2011-09-25 15:33:36 -0300

    Adds self.login and fixes self.name for LinkedInAccount

--- app/models/omnipopulus/linked_in_account.rb
+++ app/models/omnipopulus/linked_in_account.rb
@@ -1,9 +1,10 @@
 module Omnipopulus
   class LinkedInAccount < LoginAccount
     def assign_account_info(auth_hash)
+      self.login               = auth_hash['user_info']['nickname']
       self.remote_account_id   = auth_hash['uid']
       self.picture_url         = auth_hash['user_info']['image']
-      self.name                = auth_hash['user_info']['first_name']
+      self.name                = auth_hash['user_info']['name']
       self.access_token        = auth_hash['credentials']['token']
       self.access_token_secret = auth_hash['credentials']['secret']
     end
