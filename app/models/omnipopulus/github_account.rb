a5201c76482401653f0c55e95cf5d47f5bbf66a5
Leonardo Mateo <leonardomateo@gmail.com>
2011-09-25 15:46:28 -0300

    Builds picture_url for GithubAccount

--- app/models/omnipopulus/github_account.rb
+++ app/models/omnipopulus/github_account.rb
@@ -2,10 +2,12 @@ require 'digest/md5'
 
 module Omnipopulus
   class GithubAccount < LoginAccount
+
     def assign_account_info(auth_hash)
       self.remote_account_id  = auth_hash['uid']
       self.login              = auth_hash['user_info']['nickname']
       self.name               = auth_hash['user_info']['name']
+      self.picture_url        = build_picture_url auth_hash['user_info']['email']
       self.access_token       = auth_hash['credentials']['token']
     end
 
@@ -13,8 +15,8 @@ module Omnipopulus
       "http://github.com/#{self.login}"
     end
 
-    def picture_url
-      hash = Digest::MD5.hexdigest(self.email)
+    def build_picture_url(email)
+      hash = Digest::MD5.hexdigest(email)
       "http://www.gravatar.com/avatar/#{hash}"
     end
   end
